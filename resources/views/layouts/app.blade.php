aba92c2e88603189b77ecc54e0a3915e53030aa3
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-02-24 13:34:33 +0700

    update feat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href="{{ asset('Company/assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('Company/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ asset('Company/assets/img/logo.jpg') }}" rel="icon">
+    <link href="{{ asset('Company/assets/img/logo.jpg') }}" rel="apple-touch-icon">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,25 +9,8 @@
                 <li><a href="{{ url('/') }}" class="{{ request()->is('/') ? 'active' : '' }}">Home</a></li>
                 <li class="dropdown">
                     <a href="{{ url('/about') }}" class="{{ request()->is('about*') ? 'active' : '' }}">
-                        <span>About</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
+                        <span>About</span>
                     </a>
-                    <ul>
-                        <li><a href="{{ url('/team') }}" class="{{ request()->is('team') ? 'active' : '' }}">Team</a>
-                        </li>
-                        <li><a href="{{ url('/testimonials') }}"
-                                class="{{ request()->is('testimonials') ? 'active' : '' }}">Testimonials</a></li>
-                        <li class="dropdown">
-                            <a href="#"><span>Deep Dropdown</span> <i
-                                    class="bi bi-chevron-down toggle-dropdown"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Dropdown 1</a></li>
-                                <li><a href="#">Deep Dropdown 2</a></li>
-                                <li><a href="#">Deep Dropdown 3</a></li>
-                                <li><a href="#">Deep Dropdown 4</a></li>
-                                <li><a href="#">Deep Dropdown 5</a></li>
-                            </ul>
-                        </li>
-                    </ul>
                 </li>
                 <li><a href="{{ url('/services') }}"
                         class="{{ request()->is('services') ? 'active' : '' }}">Services</a></li>
@@ -35,8 +18,9 @@
                         class="{{ request()->is('portfolio') ? 'active' : '' }}">Portfolio</a></li> --}}
                 <li><a href="{{ url('/pricing') }}" class="{{ request()->is('pricing') ? 'active' : '' }}">Pricing</a>
                 </li>
-                {{-- <li><a href="{{ url('/blog') }}" class="{{ request()->is('blog') ? 'active' : '' }}">Blog</a></li>
-                --}}
+                <li><a href="{{ url('/presensi') }}" class="{{ request()->is('blog') ? 'active' : '' }}">presensi</a>
+                </li>
+
                 <li><a href="{{ url('/contact') }}" class="{{ request()->is('contact') ? 'active' : '' }}">Contact</a>
                 </li>
             </ul>
